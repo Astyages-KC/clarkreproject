a5cb6620706f68aa171a256ad674a9405bf35138
Kevin Clark <naeaman@gmail.com>
2019-11-20 15:10:06 -0700

    Nov 20th push

--- client/src/components/ForRent.js
+++ client/src/components/ForRent.js
@@ -1,16 +1,17 @@
 import React, { useState, useEffect } from "react";
 import moment from "moment";
 import axios from "axios";
+import { Carousel } from "react-responsive-carousel";
+import styles from "react-responsive-carousel/lib/styles/carousel.min.css";
 
 function ForRent() {
-  //   const { posts } = useContext(UserContext);
-  const [ posts, setPosts ] = useState([]);
+  const [posts, setPosts] = useState([]);
 
   useEffect(() => {
     axios
       .get("/public/rent")
       .then(res => {
-          setPosts(res.data)         
+        setPosts(res.data);
       })
       .catch(err => console.log(err.data));
   }, []);
@@ -18,23 +19,48 @@ function ForRent() {
   const mappedposts = posts.map((post, index) => (
     <div key={index} className="propertyBox">
       <div className="overlay-prop">
-      <div className="property-list-styling">
-      <p>
-        Address: {post.streetAddress}, {post.city}, {post.state} {post.zipCode}
-      </p>
-      <p>For Sale or Rent: {post.forRentOrSale}</p>
-      <p>Price: {post.price}</p>
-      <p>Listed on: {moment(post.createdOn).format("MMMM Do YYYY")}</p>
-      <p>Created by: {post.username}</p>
-      <img className="home-img" src={post.imageUrl} alt="logo" />
-      </div>
+        <div className="property-list-styling">
+          <p>
+            Address: {post.streetAddress}, {post.city}, {post.state}{" "}
+            {post.zipCode}
+          </p>
+          <p>For Sale or Rent: {post.forRentOrSale}</p>
+          <p>Price: {post.price}</p>
+          <p>Listed on: {moment(post.createdOn).format("MMMM Do YYYY")}</p>
+          <p>Created by: {post.username}</p>
+          <img className="home-img" src={post.imageUrl} alt="logo" />
+        </div>
       </div>
     </div>
   ));
-  return <div>
-    <h1 className="rent-sale-titles">Properties for Rent</h1> 
-  {mappedposts}
-  </div>;
+  return (
+    <div>
+      <h1 className="rent-sale-titles">Properties for Rent</h1>
+      {mappedposts}
+      <br />
+      <br />
+      <Carousel
+        autoPlay
+        interval={3000}
+        showThumbs={false}
+        infiniteLoop={true}
+        className="home-carousel"
+      >
+        {mappedposts}
+      </Carousel>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+    </div>
+  );
 }
 
 export default ForRent;
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
-import { Carousel } from "react-responsive-carousel";
-import styles from "react-responsive-carousel/lib/styles/carousel.min.css";
+// import { Carousel } from "react-responsive-carousel";
+// import styles from "react-responsive-carousel/lib/styles/carousel.min.css";
 
 
 
@@ -10,12 +10,9 @@ const Home = () => {
   <div className="home-page-styles">
     <div className="overlay">
       <h1 className="title-header" >Clark Real Estate</h1>
-      <Carousel autoPlay interval={3000} showThumbs={false} infiniteLoop={true} className="home-carousel">
-        <div style={{ height: "200px", color: "#fff" }}>this is slide 1</div>
-        <div style={{ height: "200px", color: "#fff" }}>this is slide 2</div>
-        <div style={{ height: "200px", color: "#fff" }}>this is slide 3</div>
-      </Carousel>
+      
       </div>
+      
   </div>
   )
 };
